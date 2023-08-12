b6468d5cca964e8581a43cb1238f390beb779cac
muksalbakrie <muksalbakrie@gmail.com>
2023-08-12 14:04:21 +0700

    Radius Map terdekat pada rooms

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -7,6 +7,7 @@ use App\Models\Kegiatan;
 use App\Models\Olt;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\DataTables;
@@ -16,43 +17,63 @@ class KegiatanController extends Controller
 {
     public function home()
     {
-        $data['kegiatan'] = Kegiatan::orderBy('id','ASC')->get();
+        $data['kegiatan'] = Kegiatan::orderBy('id', 'ASC')->get();
         $data['olt'] = Olt::all();
         return view('rooms.index', $data);
     }
 
+    public function reload_olt(Request $request)
+    {
+        $lat = $request->lat;
+        $lon = $request->lng;
+
+        $olts = Olt::select(
+            "olts.id",
+            "olts.nama_olt",
+            DB::raw("6371 * acos(cos(radians(" . $lat . ")) 
+                        * cos(radians(olts.lat)) 
+                        * cos(radians(olts.lng) - radians(" . $lon . ")) 
+                        + sin(radians(" . $lat . ")) 
+                        * sin(radians(olts.lat))) AS distance")
+        )->orderBy("distance", "ASC")
+            ->having('distance', '<', 1.0) //radius
+            ->get();
+
+        return $olts;
+    }
+
     public function daily()
     {
-       
 
-        if(auth()->user()->can('admin read')){
-              $daily = Daily::with(['user','jenis_kegiatan']);
+
+        if (auth()->user()->can('admin read')) {
+            $daily = Daily::with(['user', 'jenis_kegiatan']);
         }
-        if(auth()->user()->can('user read')){
-            $daily = Daily::with(['user','jenis_kegiatan'])
-            ->where('user_id', Auth()->user()->id)
-        ->orderBy('id', 'desc')->get();
+        if (auth()->user()->can('user read')) {
+            $daily = Daily::with(['user', 'jenis_kegiatan'])
+                ->where('user_id', Auth()->user()->id)
+                ->orderBy('id', 'desc')->get();
         }
 
         return DataTables::of($daily)
             ->addIndexColumn()
-             ->addColumn('created_at', function ($daily) {
+            ->addColumn('created_at', function ($daily) {
                 $formatDate = Carbon::createFromFormat('Y-m-d H:i:s', $daily->created_at)->format('d-m-Y');
                 return $formatDate;
             })
-            ->addColumn('gambar', function($daily){
+            ->addColumn('gambar', function ($daily) {
                 $images = asset("storage/files/$daily->gambar");
-                return '<img src="'.$images.'" style="height: 100px; width: 150px;"/>';
+                return '<img src="' . $images . '" style="height: 100px; width: 150px;"/>';
             })
-            
+
             ->addColumn('action', function ($daily) {
                 return '
                 <a href="javascript:void(0)" class="btn btn-outline-success btn-icon ml-2 view" data-id="' . $daily->id . '"><i class="ph-eye"></i></a>
                 <a href="javascript:void(0)" class="btn btn-outline-primary btn-icon ml-2 edit" data-id="' . $daily->id . '"><i class="ph-pencil-simple"></i></a>
                 <a href="javascript:void(0)" class="btn btn-outline-danger btn-icon ml-2 delete" data-id="' . $daily->id . '"><i class="ph-trash"></i></a>';
             })
-            
-            ->rawColumns(['action','gambar'])
+
+            ->rawColumns(['action', 'gambar'])
             ->make(true);
     }
 
@@ -88,15 +109,15 @@ class KegiatanController extends Controller
             $path = 'files/';
             $gambar = $request->file('gambar');
 
-            $format_name_images = time().'.'.$gambar->extension();
+            $format_name_images = time() . '.' . $gambar->extension();
             // $resize = Image::make($gambar);
             // $resize->fit(600)->save($gambar);
 
-            $gambar->storeAs($path, $format_name_images,'public');
+            $gambar->storeAs($path, $format_name_images, 'public');
 
             // Image::make($gambar)->resize(150,150)->save(storage_path('app/' .$format_name_images));
 
-            
+
             $ajax = new Daily();
             $ajax->user_id = auth()->user()->id;
             $ajax->lat = $request->input('latNow');
@@ -118,8 +139,8 @@ class KegiatanController extends Controller
 
     {
         // $daily = Daily::all();
-        $daily = Daily::with(['user','jenis_kegiatan'])->first(); 
-       
+        $daily = Daily::with(['user', 'jenis_kegiatan'])->first();
+
         if ($daily) {
             return response()->json([
                 'status' => 200,
@@ -152,7 +173,7 @@ class KegiatanController extends Controller
 
     public function update(Request $request, string $id)
     {
-         $rule = [
+        $rule = [
             'edit_kategori' => 'required',
             'edit_olt' => 'required',
             'edit_kegiatan' => 'required'
@@ -175,23 +196,20 @@ class KegiatanController extends Controller
 
             $ajax = Daily::find($id);
 
-            if($ajax)
-            {
+            if ($ajax) {
 
                 $ajax->kategori_dinas = $request->input('edit_kategori');
                 $ajax->nama_olt = $request->input('edit_olt');
                 $ajax->kegiatan_id = $request->input('edit_kegiatan');
                 $ajax->catatan = $request->input('edit_catatan');
 
-                if($request->hasFile('edit_gambar'))
-                {
-                    $path = 'images/'.$ajax->gambar;
-                    if(File::exists($path))
-                    {
+                if ($request->hasFile('edit_gambar')) {
+                    $path = 'images/' . $ajax->gambar;
+                    if (File::exists($path)) {
                         File::delete($path);
                     }
                     $edit_gambar = $request->file('edit_gambar');
-                    $format_name_edit_gambar = time().'.'.$edit_gambar->extension();
+                    $format_name_edit_gambar = time() . '.' . $edit_gambar->extension();
                     $edit_gambar->move('images/', $format_name_edit_gambar);
                     $ajax->gambar = $format_name_edit_gambar;
                 }
@@ -201,9 +219,7 @@ class KegiatanController extends Controller
                     'status' => 200,
                     'message' => 'Data updates successfully',
                 ]);
-            }
-            else
-            {
+            } else {
                 return response()->json([
                     'status' => 404,
                     'message' => 'Data not found',
@@ -215,8 +231,8 @@ class KegiatanController extends Controller
     public function destroy($id)
     {
         $daily = Daily::find($id);
-        $deletedFile  = File::delete("images/".$daily->gambar);
-        if(File::exists($deletedFile)) {
+        $deletedFile  = File::delete("images/" . $daily->gambar);
+        if (File::exists($deletedFile)) {
             File::delete($deletedFile);
         }
         $daily->delete();
