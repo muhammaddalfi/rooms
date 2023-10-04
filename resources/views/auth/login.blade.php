239b14bc25198128fd8c7f8c8c24b8f5970e8953
Muhammad Alfi <muhammaddalfi@gmail.com>
2023-10-04 12:03:59 +0700

    title page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Monster Jabar</title>
+    <title>Monster - Monitoring Sales Cluster</title>
 
     <!-- Global stylesheets -->
     <link href="{{ asset('assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
