0a9a54a30a4fcc77586b9d55e3ff537b0501e7e6
Muhammad Alfi <muhammaddalfi@gmail.com>
2023-11-03 12:09:12 +0700

    Fix error laporan muntang

--- app/Http/Controllers/LaporanDebt.php
+++ app/Http/Controllers/LaporanDebt.php
@@ -17,25 +17,37 @@ class LaporanDebt extends Controller
     public function search(Request $request)
     {
         if($request->ajax())
-        {
-               $data = Baddeb::where('user_id', auth()->user()->id)
-               ->whereNotIn('status_bayar', ['close'])->get();
+        {        
+        if(Auth::user()->hasRole(['admin','management'])){
+            $data = Baddeb::whereNotIn('status_bayar', ['close'])->get();
                if($request->filled('from_date') && $request->filled('end_date'))
                {
-                    // $data_raw = "SELECT *
-                    //             FROM baddebs 
-                    //             WHERE status_bayar NOT IN('close')
-                    //             AND created_at BETWEEN ? AND ?
-                    //             ORDER BY id DESC";
                     $data_raw = "SELECT b.id, b.nama_pelanggan,b.id_pln, b.layanan, b.status_bayar, 
                                     (SELECT u.name FROM users u WHERE id = b.user_id) AS nama_petugas
                                     FROM baddebs b
                                     WHERE status_bayar IN('close')
-                                    AND created_at BETWEEN ? AND ?
+                                    AND created_at BETWEEN ? AND ? 
+                                    ORDER BY id DESC";
+                    $data = DB::select($data_raw,[$request->from_date,$request->end_date]);
+               }
+        }
+
+        if(Auth::user()->hasRole(['collection'])){
+            $data = Baddeb::where('user_id', auth()->user()->id)
+               ->whereNotIn('status_bayar', ['close'])->get();
+               if($request->filled('from_date') && $request->filled('end_date'))
+               {
+                    $data_raw = "SELECT b.id, b.nama_pelanggan,b.id_pln, b.layanan, b.status_bayar, 
+                                    (SELECT u.name FROM users u WHERE id = b.user_id) AS nama_petugas
+                                    FROM baddebs b
+                                    WHERE status_bayar IN('close')
+                                    AND created_at BETWEEN ? AND ? 
                                     AND b.user_id = '".Auth()->user()->id."'
                                     ORDER BY id DESC";
                     $data = DB::select($data_raw,[$request->from_date,$request->end_date]);
                }
+        }
+               
             
             return DataTables::of($data)
             ->addIndexColumn()
