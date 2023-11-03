aa80707a7be26789f159c5e6126affb0e2b99bb5
Muhammad Alfi <muhammaddalfi@gmail.com>
2023-11-03 14:39:21 +0700

    status pending laporan view

--- app/Http/Controllers/LaporanDebt.php
+++ app/Http/Controllers/LaporanDebt.php
@@ -20,13 +20,13 @@ class LaporanDebt extends Controller
         if($request->ajax())
         {        
         if(Auth::user()->hasRole(['admin','management'])){
-            $data = Baddeb::whereNotIn('status_bayar', ['close'])->get();
+            $data = Baddeb::whereIn('status_bayar', ['close','pending'])->get();
                if($request->filled('from_date') && $request->filled('end_date'))
                {
                     $data_raw = "SELECT b.id, b.nama_pelanggan,b.id_pln, b.layanan, b.status_bayar, 
                                     (SELECT u.name FROM users u WHERE id = b.user_id) AS nama_petugas
                                     FROM baddebs b
-                                    WHERE status_bayar IN('close')
+                                    WHERE status_bayar IN('close','pending')
                                     AND created_at BETWEEN ? AND ? 
                                     ORDER BY id DESC";
                     $data = DB::select($data_raw,[$request->from_date,$request->end_date]);
@@ -35,13 +35,13 @@ class LaporanDebt extends Controller
 
         if(Auth::user()->hasRole(['collection'])){
             $data = Baddeb::where('user_id', auth()->user()->id)
-               ->whereNotIn('status_bayar', ['close'])->get();
+               ->whereIn('status_bayar', ['close','pending'])->get();
                if($request->filled('from_date') && $request->filled('end_date'))
                {
                     $data_raw = "SELECT b.id, b.nama_pelanggan,b.id_pln, b.layanan, b.status_bayar, 
                                     (SELECT u.name FROM users u WHERE id = b.user_id) AS nama_petugas
                                     FROM baddebs b
-                                    WHERE status_bayar IN('close')
+                                    WHERE status_bayar IN('close','pending')
                                     AND created_at BETWEEN ? AND ? 
                                     AND b.user_id = '".Auth()->user()->id."'
                                     ORDER BY id DESC";
@@ -55,6 +55,9 @@ class LaporanDebt extends Controller
             ->addColumn('status', function ($data) {
                if($data->status_bayar == 'close'){
                     return '<span class="badge bg-success">Close</span>';
+                }
+                else if($data->status_bayar == 'pending'){
+                    return '<span class="badge bg-warning">Pending</span>';
                 }
             })
             // ->addColumn('action', function ($data) {
